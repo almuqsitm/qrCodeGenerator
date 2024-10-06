931960373bbb60a84d3a7359137110189bffab7f
Mohammed Al-Muqsit <almuqsit101@gmail.com>
2024-10-05 18:58:40 -0700

    Mostly finished: added coloring and functionality for the button

--- README.md
+++ README.md
@@ -2,11 +2,7 @@
 
 ## Overview
 
-This is a very simple python code that generates a QR code for a link or search. Currently, running it will generate a QR code for my Github and uncommenting the second to last line will allow the user to download the QR code as an .png file. This is a work in progress and there will be a GUI in the future to customize QR code.
-
-## Next Steps
-- Input field to take in the link that a user wants
-- checkbox to give the option to download as a .png file and/or open a QR code for the given link
+This is a very simple python code that generates a QR code for a link or search using Tkinter.
 
 ## Getting Started
 
--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-from tkinter import *
 import qrcode
-from PIL import Image
 import tkinter as tk
 
-from PIL.ImageColor import colormap
+
 
 
 class MyGUI:
     def __init__(self):
         self.root = tk.Tk()
-        self.center_window(500,500)
+        self.center_window(500,200)
         #self.root.geometry("400x400")
         self.root.title("QR Code Generator")
+        self.root.config(bg="black")
 
         #Label
         self.root.grid_columnconfigure(0, weight=1)
@@ -19,25 +18,36 @@ class MyGUI:
         self.label = tk.Label(self.root,
                               text="Enter QR Link:",
                               font=('Ariel', 13),
-                              pady=30
+                              fg="#7bf71b",
+                              pady=30,
+                              bg="black"
                               )
         self.label.grid(row=0,column=0)
 
         #entry -- where they will put link for QR
         self.entry = tk.Entry(self.root,
                               font=('Ariel', 16),
+                              bg='#444647',
+                              fg="#7bf71b"
                               )
         self.entry.grid(row=0,column=1)
 
         #checkbox
         self.check1 = tk.IntVar()
         self.check2 = tk.IntVar()
+        #self.check3 = tk.IntVar()
+
         self.checkbox = tk.Checkbutton(self.root,
                                        text="Download",
                                        variable=self.check1,
                                        onvalue=1,
                                        offvalue=0,
-                                       command=self.downloadQR)
+                                       fg='#7bf71b',
+                                       bg="black",
+                                       activebackground="black",
+                                       activeforeground="#7bf71b",
+                                       selectcolor="white"
+                                       )
         self.checkbox.grid(row=1, column=0)
 
         self.checkbox2 = tk.Checkbutton(self.root,
@@ -45,20 +55,54 @@ class MyGUI:
                                        variable=self.check2,
                                        onvalue=1,
                                        offvalue=0,
-                                       command=self.openQR)
+                                       fg="#7bf71b",
+                                        bg="black",
+                                        activebackground="black",
+                                        activeforeground="#7bf71b",
+                                        selectcolor="white"
+                                       )
         self.checkbox2.grid(row=1, column=1)
 
-
+        #generate button
+        self.button = tk.Button(self.root,
+                                text="Generate",
+                                font=('Ariel', 16),
+                                command=self.generateQR,
+                                bg="#444647",
+                                fg="#7bf71b",
+                                bd=5,
+                                relief="raised"
+                                )
+        self.button.grid(row=2, column=0, columnspan=2, pady=20)
 
         #run GUI
         self.root.mainloop()
 
-    def downloadQR(self):
+    '''def downloadQR(self):
         if self.check1.get() == 1:
             print("Download")
     def openQR(self):
         if self.check2.get() == 1:
-            print("Open QR")
+            print("Open QR")'''
+    def generateQR(self):
+        #print("button clicked", self.entry.get())
+
+        data = self.entry.get()
+        qr = qrcode.QRCode(version=1,
+                           box_size=10,
+                           border=5)
+
+        qr.add_data(data)
+        qr.make(fit=True)
+        img = qr.make_image(fill_color='black', back_color='white')
+
+        if self.check1.get() == 1:
+            img.save('MyQRCode.png')
+        if self.check2.get() == 1:
+            img.show()
+
+
+        self.root.destroy()
 
     def center_window(self, width, height):
         # Get the screen width and height
@@ -72,7 +116,7 @@ class MyGUI:
         # Set the window geometry to the new coordinates
         self.root.geometry(f'{width}x{height}+{x}+{y}')
 
-data = "https://github.com/almuqsitm"
+'''data = "https://github.com/almuqsitm"
 
 qr = qrcode.QRCode(version=1,
                    box_size=10,
@@ -82,7 +126,7 @@ qr.add_data(data)
 
 qr.make(fit=True)
 img = qr.make_image(fill_color='black',back_color='white')
-
+'''
 #img.save('MyQRCode2.png')
 #img.show()
 #
